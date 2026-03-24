a5a86c435811ca73c56a8a605c328fd25cd293fb
Amol Kalaskar <50816350+amolkalaskar@users.noreply.github.com>
2026-03-25 00:33:01 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,15 @@
-module "function_app" {
-  source = "./modules/function_app"
-  resource_group_name = var.rg-name
-  location            = var.location
-  function_app_name = var.function_app_name
+module "rg" {
+  source = "./modules/resource_group"
+
+  name     = var.resource_group_name
+  location = var.location
 }
 
+module "function_app" {
+  source = "./modules/function_app"
 
+  resource_group_name = module.rg.name
+  location            = module.rg.location
+
+  function_app_name = var.function_app_name
+}
